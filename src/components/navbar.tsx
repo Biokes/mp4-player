b516cf3017203186bb284b8072ffd25b22bbbc46
Biokes <106444765+Biokes@users.noreply.github.com>
2024-11-24 22:12:47 +0100

    update

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar(){
     return (
         <View>
             <Icon name="video-library" size={50} color="green" />
-            <Text tw={'text-xl font-bold text-blue-600'}>Hello, NativeWind!</Text>
+            <Text tw={'text-xl font-bold text-blue-600'}>Spedia Tube</Text>
         </View>
     )
 }
